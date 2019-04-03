f7829c12da2fbbb0f01e2c64e868aa9cc50e8d0e
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-03 00:33:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
   
   extend Findable
   
@@ -40,7 +41,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
